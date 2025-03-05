3f33be09a9e6657c888e67f447bfe209347e8084
student <student@tp.local>
2025-03-05 21:53:44 +0300

    lesson_parcing_hh

---
+++ parcing_hh.py
@@ -0,0 +1,27 @@
+import requests
+
+def get_python_vacancy():
+    url = 'https://api.hh.ru/vacancies'
+    params = {
+        "per_page": 100,
+        "text": "python",
+        "area": 2
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    found = data['found']
+    pages = data['pages']
+
+    vacancies = []
+
+
+    for i in range(0, pages+1):
+        params['page'] = i
+        response = requests.get(url, params=params)
+        data = response.json()
+        vacancies.extend(data['items'])
+
+    pass
+
+get_python_vacancy()
